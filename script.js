1ac28c3e8f14bff5e9d6d0075e9ea8f3314add8a
zhanglanpo123 <161694245+zhanglanpo123@users.noreply.github.com>
2024-03-14 21:06:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function sayHello(){
+  var response = prompt("what is your name");
+  alert('Hello,'+ response +', have a nice day!');
+}
+//sayHello();
+function showTopic(){
+  var x =document.getElementById('demo');
+  x.style. dontSize = '25px';
+  x.style. color ='red';
+}
